146ca1c6bc0e2ddc5693e27ad290a3ca99de543d
Diaz Raphael <diazyarribas.raphael@gmail.com>
2022-07-04 15:34:45 +0200

    app_fonctionnelle

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-from flask import Flask, render_template
-import sqlite3
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+@app.route('/')
+def hello():
+    return 'Hello, World!'
