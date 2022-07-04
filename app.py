ef9fc56ea8b017af310a86101ea3aa33b1ad3107
Diaz Raphael <73168986+RaphaelDiaz94@users.noreply.github.com>
2022-07-04 14:20:25 +0200

    add files

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
