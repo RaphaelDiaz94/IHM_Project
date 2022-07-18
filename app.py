db7fca40cdae0dd8744594cb6f1ba14574c01972
Diaz Raphael <diazyarribas.raphael@gmail.com>
2022-07-18 18:26:13 +0200

    bootstrapp

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 
 from flask import Flask, render_template,request
-from flask_bootstrap import Bootstrap4
+from flask_bootstrap import Bootstrap
 import sqlite3
 import os
 import psycopg2
 
 app = Flask(__name__)
-Bootstrap4(app)
+Bootstrap(app)
 
 @app.route('/')
 def index():
