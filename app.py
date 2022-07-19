a229f503605dce9642d56d3559529bdcfbe2152a
Diaz Raphael <diazyarribas.raphael@gmail.com>
2022-07-19 17:40:45 +0200

    Test de récupération de valeur dans la page index depuis la bdd

--- app.py
+++ app.py
@@ -9,11 +9,8 @@ app = Flask(__name__)
 Bootstrap(app)
 
 @app.route('/')
+
 def index():
-    return render_template('index.html')
-
-def init_db():
-
     conn = psycopg2.connect("postgres://owshwcafnfsgsx:2b4cf5ade3fb7b2f25e3f1b66cd29d5a7e420fdd1d51b4c01df4b6086f1db630@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d5arg29ce13853", sslmode='require')
     print("connexion ok")
     cur = conn.cursor()
@@ -22,5 +19,6 @@ def init_db():
     val = cur.fetchone()
     print(val)
     print("type de val: ", type(val))
-    return cur,val
+    return render_template('index.html', val=val, cur=cur)
+
 
