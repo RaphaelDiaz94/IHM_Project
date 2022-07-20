b47fa83c5923d9174a2825f295e06c43112521be
Diaz Raphael <diazyarribas.raphael@gmail.com>
2022-07-20 11:02:52 +0200

    maj des tests

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ import psycopg2
 def tests():
 
     conn = psycopg2.connect("postgres://owshwcafnfsgsx:2b4cf5ade3fb7b2f25e3f1b66cd29d5a7e420fdd1d51b4c01df4b6086f1db630@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d5arg29ce13853", sslmode='require')
-    assert conn == True
+    
 
     cur = conn.cursor()
     cur.execute("DROP TABLE tests IF EXISTS;")
