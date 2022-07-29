2d128257d89fc62f77d5815882c8cfa61d381049
Diaz Raphael <diazyarribas.raphael@gmail.com>
2022-07-29 13:03:31 +0200

    test

--- app.py
+++ app.py
@@ -4,13 +4,33 @@ from flask_bootstrap import Bootstrap
 import sqlite3
 import os
 import psycopg2
-
+from flask_sockets import sockets
+import logging
+import redis
+import gevent
+import boto3
+from werkzeug.utils import secure_filename
+from PIL import Image
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
 
+@app.route('/')
 def index():
+
+
+    s3 = boto3.client('s3',
+                    aws_access_key_id='AKIAVA5FQS27ZQZRC4ER',
+                    aws_secret_access_key= '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',
+                     )
+    BUCKET_NAME='myphotobucketraph'
+    for s3_object in BUCKET_NAME.objects.all():
+   
+        #Use this statement if your files are available directly in your bucket. 
+        BUCKET_NAME.download_file(s3_object.key, 'test.png')
+        img = Image.open('test.png')
+        print("OK")
+    
     conn = psycopg2.connect("postgres://owshwcafnfsgsx:2b4cf5ade3fb7b2f25e3f1b66cd29d5a7e420fdd1d51b4c01df4b6086f1db630@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d5arg29ce13853", sslmode='require')
     print("connexion ok")
     cur = conn.cursor()
@@ -19,6 +39,7 @@ def index():
     val = cur.fetchall()
     print(val)
     print("type de val: ", type(val))
+
     return render_template('index.html', val=val, cur=cur)
 
 
