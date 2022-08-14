cf072f62215cec0e4d81cf3cd56fd937a1df65e4
Diaz Raphael <diazyarribas.raphael@gmail.com>
2022-08-14 16:44:33 +0200

    test image

--- app.py
+++ app.py
@@ -31,9 +31,7 @@ def index():
         Filename=file_name,  
         Key = file_name,  
     )
-      
 
-    
     except Exception as e:
         print(e)
         print('Error downloading image')
@@ -46,11 +44,9 @@ def index():
 
     
     conn = psycopg2.connect("postgres://owshwcafnfsgsx:2b4cf5ade3fb7b2f25e3f1b66cd29d5a7e420fdd1d51b4c01df4b6086f1db630@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d5arg29ce13853", sslmode='require')
-    print("connexion ok")
     cur = conn.cursor()
     cur.execute("SELECT * FROM stats;")
     val = cur.fetchall()
-    print(val)
 
     return render_template('index.html', val=val, cur=cur , img=img)
 
