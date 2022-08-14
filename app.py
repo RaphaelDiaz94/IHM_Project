33b19f1b76b7586e238aa1d2fdcf801340923dd7
Diaz Raphael <diazyarribas.raphael@gmail.com>
2022-08-14 12:47:28 +0200

    test img

--- app.py
+++ app.py
@@ -42,18 +42,14 @@ def index():
     #des=r'/Users/raphaeldiaz/Desktop/IHM_Projet/static/images/Users_raphaeldiaz_Desktop_test.png'
     #os.rename(src,des)
 
-    print(img)
-    print("OK")
 
     
     conn = psycopg2.connect("postgres://owshwcafnfsgsx:2b4cf5ade3fb7b2f25e3f1b66cd29d5a7e420fdd1d51b4c01df4b6086f1db630@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d5arg29ce13853", sslmode='require')
     print("connexion ok")
     cur = conn.cursor()
-    print("cursor ok")
     cur.execute("SELECT * FROM stats;")
     val = cur.fetchall()
     print(val)
-    print("type de val: ", type(val))
 
     return render_template('index.html', val=val, cur=cur , img=img)
 
