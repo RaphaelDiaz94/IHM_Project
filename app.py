ac4fc793ba1a3f2d88463d19c462e18f04480907
Diaz Raphael <diazyarribas.raphael@gmail.com>
2022-08-15 16:40:24 +0200

    ajout de page aide

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def index():
             ClientMethod='get_object', 
             Params={'Bucket': BUCKET_NAME, 'Key': file_name},
         ExpiresIn=3600)
-        print(type(filename))
 
     except Exception as e:
         print(e)
