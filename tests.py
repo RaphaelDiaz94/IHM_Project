360a09a7535dc4ec2d100dc406b166ec2cf50ac1
Diaz Raphael <diazyarribas.raphael@gmail.com>
2022-09-01 22:11:36 +0200

    maj des tests

--- tests.py
+++ tests.py
@@ -10,9 +10,9 @@ def tests():
     conn = psycopg2.connect("postgres://owshwcafnfsgsx:2b4cf5ade3fb7b2f25e3f1b66cd29d5a7e420fdd1d51b4c01df4b6086f1db630@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d5arg29ce13853", sslmode='require')
     cur = conn.cursor()
     cur.execute("DROP TABLE IF EXISTS tests;")
-    cur.execute("CREATE TABLE tests (id serial PRIMARY KEY,nom_element char(50), nb_element integer,precision char(50),nomrue(50));")
-    cur.execute("INSERT INTO tests (nom_element, nb_element, precision, nomrue) VALUES (%s, %s ,%s,%s)",("trotinette",3,"10-10-2022-22-00","rue de la foi"))
-    cur.execute("INSERT INTO tests (nom_element, nb_element, precision, nomrue) VALUES (%s, %s ,%s,%s)",("avion",1, "10-10-2022-22-00","rue de la foi"))
+    cur.execute("CREATE TABLE tests (id serial PRIMARY KEY,nom_element char(50), nb_element integer,precision char(50), nomrue char(50));")
+    cur.execute("INSERT INTO tests (nom_element, nb_element, precision ,nomrue) VALUES (%s, %s ,%s ,%s)",("trotinette",3, "01-09-2022", "rue de la foi"))
+    cur.execute("INSERT INTO tests (nom_element, nb_element, precision, nomrue) VALUES (%s, %s ,%s ,%s)",("avion",1, "01-09-2022", "rue de la foi"))
 
     conn.commit()
     cur.execute("SELECT * FROM stats;")
